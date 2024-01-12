31dbad3a8a8dfdc12d6295e619cbeac4db7f06d2
“acalderon20” <acalderon20@coe.edu>
2024-01-12 00:31:55 -0600

    Initial Commit

---
+++ chatGPT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chatGPT
+//
+//  Created by Adolfo Calderon on 1/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chatGPT/chatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatGPTApp.swift
+//  chatGPT
+//
+//  Created by Adolfo Calderon on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct chatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
